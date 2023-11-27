074a6e67dca9492c99a970aab2553089fa7c4f28
Sorn Veasna@X1502ZA <sornveasna07s@gmail.com>
2023-11-27 12:14:16 +0700

    Update on   modified:   .env         new file:   app/Models/Category.php         new file:   database/migrations/2023_11_27_050958_category.php         modified:   vendor/composer/ClassLoader.php         modified:   vendor/composer/autoload_classmap.php         modified:   vendor/composer/autoload_psr4.php         modified:   vendor/composer/autoload_real.php         modified:   vendor/composer/autoload_static.php

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit5c4be4b1c01e5b5d489455617341f766
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit5c4be4b1c01e5b5d489455617341f766::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -323,8 +323,8 @@ class ComposerStaticInit5c4be4b1c01e5b5d489455617341f766
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -504,6 +504,7 @@ class ComposerStaticInit5c4be4b1c01e5b5d489455617341f766
     public static $classMap = array (
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
+        'App\\Http\\Controllers\\CategoryController' => __DIR__ . '/../..' . '/app/Http/Controllers/CategoryController.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
@@ -514,6 +515,7 @@ class ComposerStaticInit5c4be4b1c01e5b5d489455617341f766
         'App\\Http\\Middleware\\TrustHosts' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustHosts.php',
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
+        'App\\Models\\Category' => __DIR__ . '/../..' . '/app/Models/Category.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
